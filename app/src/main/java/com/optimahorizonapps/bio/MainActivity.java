5c26628388dd914c59480fc86593259a92d55c78
leonardo1313 <leonardo.novak5@gmail.com>
2021-08-30 20:55:40 +0200

    Data package added and Bio class created to bind data - variable name is working - variable hobbies NOT working
    TODO
    - fix saving and displaying hobbies

--- app/src/main/java/com/optimahorizonapps/bio/MainActivity.java
+++ app/src/main/java/com/optimahorizonapps/bio/MainActivity.java
@@ -8,17 +8,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import com.optimahorizonapps.bio.data.Bio;
 import com.optimahorizonapps.bio.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Bio bio = new Bio();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        bio.setName("Emma Harris");
+
+        binding.setBio(bio);
         binding.saveHobbiesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void saveHobbies(View v) {
-        binding.hobbiesTextView.setText(String.format("Hobbies: %s", binding.hobbiesEditText.getText().toString().trim()));
+        bio.setHobbies(String.format("Hobbies: %s", binding.hobbiesEditText.getText().toString().trim()));
+//        binding.hobbiesTextView.setText(String.format("Hobbies: %s", binding.hobbiesEditText.getText().toString().trim()));
         binding.saveHobbiesButton.setVisibility(View.VISIBLE);
 
         // hide keyboard
